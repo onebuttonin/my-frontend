1881094ff6fd7675d7317bc5958dbd1b3c5bed58
onebuttonin <onebutton.co@gmail.com>
2025-05-02 11:45:14 +0530

    wishlist login fix useEffect

--- onebutton/src/components/Wishlist/WishlistPage.jsx
+++ onebutton/src/components/Wishlist/WishlistPage.jsx
@@ -242,7 +242,7 @@
 // }
 
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Trash } from "lucide-react";
