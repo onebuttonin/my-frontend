359c5dff8407310df4aef63bccc87d41cbed89f1
onebuttonin <onebutton.co@gmail.com>
2025-08-15 21:14:11 +0530

    main image

--- onebutton/src/components/Nbottom.jsx
+++ onebutton/src/components/Nbottom.jsx
@@ -10,44 +10,124 @@
 
 // }
 
+// import React from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Autoplay } from "swiper/modules";
+// import "swiper/css";
+
+// export default function Nbottom() {
+//   const images = [
+//     "/images/WEB.jpg",
+//     "/images/WEB2.jpg",
+//   ];
+
+//   return (
+//     <div className="container mx-auto flex justify-center items-center bg-neutral-100 max-h-[450px]">
+//       <Swiper
+//         modules={[Autoplay]}
+//         loop={true}
+//         autoplay={{
+//           delay: 3000,
+//           disableOnInteraction: false,
+//         }}
+//         speed={800}
+//         slidesPerView={1}
+//         centeredSlides={true}
+//       >
+//         {images.map((src, idx) => (
+//           <SwiperSlide key={idx} className="flex justify-center items-center">
+//             <img
+//               src={src}
+//               alt={`Slide ${idx}`}
+//               className="h-full max-h-[450px] w-auto object-contain"
+//               style={{
+//                 display: "block",
+//                 margin: "0 auto", // keep horizontal center
+//               }}
+//             />
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//     </div>
+//   );
+// }
+
+
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 
 export default function Nbottom() {
-  const images = [
+  // Images for large and small screens
+  const largeScreenImages = [
     "/images/WEB.jpg",
     "/images/WEB2.jpg",
   ];
+  const smallScreenImages = [
+    "/images/main image-1.jpg",
+    "/images/main image-1.jpg",
+  ];
 
   return (
-    <div className="container mx-auto flex justify-center items-center bg-neutral-100 max-h-[450px]">
-      <Swiper
-        modules={[Autoplay]}
-        loop={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        speed={800}
-        slidesPerView={1}
-        centeredSlides={true}
-      >
-        {images.map((src, idx) => (
-          <SwiperSlide key={idx} className="flex justify-center items-center">
-            <img
-              src={src}
-              alt={`Slide ${idx}`}
-              className="h-full max-h-[450px] w-auto object-contain"
-              style={{
-                display: "block",
-                margin: "0 auto", // keep horizontal center
-              }}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="container mx-auto flex justify-center items-center bg-neutral-100">
+      {/* Large Screen Slider */}
+      <div className="hidden lg:block w-full">
+        <Swiper
+          modules={[Autoplay]}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          speed={800}
+          slidesPerView={1}
+          centeredSlides={true}
+        >
+          {largeScreenImages.map((src, idx) => (
+            <SwiperSlide key={idx} className="flex justify-center items-center">
+              <img
+                src={src}
+                alt={`Large Slide ${idx}`}
+                className="h-full max-h-[450px] w-auto object-contain"
+                style={{
+                  display: "block",
+                  margin: "0 auto",
+                }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
+      {/* Small Screen Slider */}
+      <div className="block lg:hidden w-full">
+        <Swiper
+          modules={[Autoplay]}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          speed={800}
+          slidesPerView={1}
+          centeredSlides={true}
+        >
+          {smallScreenImages.map((src, idx) => (
+            <SwiperSlide key={idx} className="flex justify-center items-center">
+              <img
+                src={src}
+                alt={`Small Slide ${idx}`}
+                className="h-full max-h-[600px] w-auto object-contain" // taller for small screens
+                style={{
+                  display: "block",
+                  margin: "0 auto",
+                }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
